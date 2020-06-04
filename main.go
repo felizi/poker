2b9ca73d1606360b69b6502a7820a5c1d366f96d
Daniel Felizi <felizi.daniel@gmail.com>
2020-06-03 21:55:42 -0300

    Impl of all hands of poker, without otimization

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 
 /*
 https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
-https://www.pokerstars.com/br/poker/games/rules/?no_redirect=1
+https://www.pokerstars.com/br/poker/games/rules
 https://br.888poker.com/how-to-play-poker/
 https://pt.wikipedia.org/wiki/P%C3%B4quer
 https://www.pokerstars.com/br/poker/games/rules/hand-rankings/
 https://www.clubedopoker.com/o-valor-das-cartas-do-baralho-de-poker/
+https://www.cardplayer.com/rules-of-poker/hand-rankings
 */
 
 const (
@@ -53,27 +54,36 @@ func main() {
 	fmt.Printf("player2: %v\n", player2)
 
 	flop := flop(remove(cards, player2[:]))
-	fmt.Printf("flop: %v\n", flop)
-
 	turn := turn(remove(cards, flop[:]))
-	fmt.Printf("turn: %v\n", turn)
-
 	river := river(remove(cards, turn[:]))
-	fmt.Printf("river: %v\n", river)
-
 	fmt.Printf("community cards: %v\n", []Card{flop[0], flop[1], flop[2], turn[0], river[0]})
 
-	weight, t := twoPair(player1, flop, turn, river)
-	fmt.Printf("player1 - two pair: \t %v %v\n", weight, t)
-	weight, c := onePair(player1, flop, turn, river)
-	fmt.Printf("player1 - one pair: \t %v %v\n", weight, c)
-	fmt.Printf("player1 - high card: \t %v\n", highCard(player1, flop, turn, river))
+	check("Player1", player1, flop, turn, river)
+	check("Player2", player2, flop, turn, river)
+}
 
-	weight, t = twoPair(player2, flop, turn, river)
-	fmt.Printf("player2 - two pair: \t %v %v\n", weight, t)
-	weight, c = onePair(player2, flop, turn, river)
-	fmt.Printf("player2 - one pair: \t %v %v\n", weight, c)
-	fmt.Printf("player2 - high card: \t %v\n", highCard(player2, flop, turn, river))
+func check(name string, hand [2]Card, flop [3]Card, turn, river [1]Card) {
+	fmt.Printf("%v: \t %v\n", name, concatenate(hand, flop, turn, river))
+	w, rf := royalFlush(hand, flop, turn, river)
+	fmt.Printf("%v - royal flush: \t %v %v\n", name, w, rf)
+	w, sf := straightFlush(hand, flop, turn, river)
+	fmt.Printf("%v - straight flush: \t %v %v\n", name, w, sf)
+	w, foak := fourOfAKind(hand, flop, turn, river)
+	fmt.Printf("%v - four of a kind: \t %v %v\n", name, w, foak)
+	w, fh := fullHouse(hand, flop, turn, river)
+	fmt.Printf("%v - full house: \t %v %v\n", name, w, fh)
+	w, f := flush(hand, flop, turn, river)
+	fmt.Printf("%v - flush: \t %v %v\n", name, w, f)
+	w, s := straight(hand, flop, turn, river)
+	fmt.Printf("%v - straight: \t %v %v\n", name, w, s)
+	w, t := threeOfAKind(hand, flop, turn, river)
+	fmt.Printf("%v - three of a kind: \t %v %v\n", name, w, t)
+	w, p := twoPair(hand, flop, turn, river)
+	fmt.Printf("%v - two pair: \t %v %v\n", name, w, p)
+	w, c := onePair(hand, flop, turn, river)
+	fmt.Printf("%v - one pair: \t %v %v\n", name, w, c)
+	w, h := highCard(hand, flop, turn, river)
+	fmt.Printf("%v - high card: \t %v  %v\n", name, w, h)
 }
 
 func remove(items []Card, exclusions []Card) []Card {
@@ -117,6 +127,198 @@ func random(x []Card) Card {
 	return x[rand.Intn(len(x))]
 }
 
+func royalFlush(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[5]Card) {
+	cards := concatenate(hand, flop, turn, river)
+
+	var result [5]Card
+	var weight int
+	var lastWeight int
+	var idx int
+	var suit string
+	for x := 0; x < len(cards); x++ {
+		if idx == 0 && cards[x].ID == "A" || lastWeight-1 == cards[x].Weight && suit == cards[x].Suit {
+			lastWeight = cards[x].Weight
+			weight += cards[x].Weight
+			result[idx] = cards[x]
+			idx++
+			suit = cards[x].Suit
+		} else {
+			lastWeight = 0
+			weight = 0
+			result = [5]Card{}
+			idx = 0
+			suit = ""
+		}
+		if idx >= 5 {
+			return weight, &result
+		}
+	}
+
+	return 0, nil
+}
+
+func straightFlush(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[5]Card) {
+	cards := concatenate(hand, flop, turn, river)
+
+	var result [5]Card
+	var weight int
+	var lastWeight int
+	var idx int
+	var suit string
+	for x := 0; x < len(cards); x++ {
+		if x == 0 || lastWeight-1 == cards[x].Weight && suit == cards[x].Suit {
+			lastWeight = cards[x].Weight
+			weight += cards[x].Weight
+			result[idx] = cards[x]
+			idx++
+			suit = cards[x].Suit
+		} else {
+			lastWeight = 0
+			weight = 0
+			result = [5]Card{}
+			idx = 0
+			suit = ""
+		}
+		if idx >= 5 {
+			return weight, &result
+		}
+	}
+
+	return 0, nil
+}
+
+func fourOfAKind(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[4]Card) {
+	m := groupByID(hand, flop, turn, river)
+	var matches []string
+	for k, v := range m {
+		if len(v) == 4 {
+			matches = append(matches, k)
+		}
+	}
+	if len(matches) == 1 {
+		var result [4]Card
+		var weight int
+		for i := 0; i < len(matches); i++ {
+			for x := 0; x < len(m[matches[i]]); x++ {
+				c := m[matches[i]][x]
+				weight += c.Weight
+				result[x] = c
+			}
+		}
+		return weight, &result
+	}
+
+	return 0, nil
+}
+
+func fullHouse(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[5]Card) {
+	m := groupByID(hand, flop, turn, river)
+	var threePair []string
+	var twoPair []string
+	for k, v := range m {
+		if len(v) == 3 {
+			threePair = append(threePair, k)
+		}
+		if len(v) == 2 {
+			twoPair = append(twoPair, k)
+		}
+	}
+	if len(twoPair) == 1 && len(threePair) == 1 {
+		cards := append(twoPair, threePair...)
+		var result [5]Card
+		var weight int
+		var idx int
+		for i := 0; i < len(cards); i++ {
+			for x := 0; x < len(m[cards[i]]); x++ {
+				c := m[cards[i]][x]
+				weight += c.Weight
+				result[idx] = c
+				idx++
+			}
+		}
+		return weight, &result
+	}
+
+	return 0, nil
+}
+
+func flush(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[5]Card) {
+	m := groupBySuit(hand, flop, turn, river)
+
+	var matches []string
+	for k, v := range m {
+		if len(v) == 5 {
+			matches = append(matches, k)
+		}
+	}
+	if len(matches) == 1 {
+		var result [5]Card
+		var weight int
+		for i := 0; i < len(matches); i++ {
+			for x := 0; x < len(m[matches[i]]); x++ {
+				c := m[matches[i]][x]
+				weight += c.Weight
+				result[x] = c
+			}
+		}
+		return weight, &result
+	}
+
+	return 0, nil
+
+}
+
+func straight(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[5]Card) {
+	cards := concatenate(hand, flop, turn, river)
+
+	var result [5]Card
+	var weight int
+	var lastWeight int
+	var idx int
+	for x := 0; x < len(cards); x++ {
+		if x == 0 || lastWeight-1 == cards[x].Weight {
+			lastWeight = cards[x].Weight
+			weight += cards[x].Weight
+			result[idx] = cards[x]
+			idx++
+		} else {
+			lastWeight = 0
+			weight = 0
+			result = [5]Card{}
+			idx = 0
+		}
+		if idx >= 5 {
+			return weight, &result
+		}
+	}
+
+	return 0, nil
+}
+
+func threeOfAKind(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[3]Card) {
+	m := groupByID(hand, flop, turn, river)
+	var matches []string
+	for k, v := range m {
+		if len(v) == 3 {
+			matches = append(matches, k)
+		}
+	}
+	if len(matches) == 1 {
+		var result [3]Card
+		var weight int
+		for i := 0; i < len(matches); i++ {
+			for x := 0; x < len(m[matches[i]]); x++ {
+				c := m[matches[i]][x]
+				weight += c.Weight
+				result[x] = c
+			}
+		}
+		return weight, &result
+	}
+
+	return 0, nil
+}
+
 func twoPair(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[2][2]Card) {
 	m := groupByID(hand, flop, turn, river)
 	var matches []string
@@ -142,15 +344,38 @@ func twoPair(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[2][2]Card)
 }
 
 func onePair(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[2]Card) {
-	cards := concatenate(hand, flop, turn, river)
-	var card *Card
-	for i := 0; i < len(cards); i++ {
-		if card != nil && card.ID == cards[i].ID {
-			return card.Weight + cards[i].Weight, &[2]Card{*card, cards[i]}
+	m := groupByID(hand, flop, turn, river)
+	var matches []string
+	for k, v := range m {
+		if len(v) == 2 {
+			matches = append(matches, k)
 		}
-		card = &cards[i]
 	}
+	if len(matches) == 1 {
+		var result [2]Card
+		var weight int
+		for i := 0; i < len(matches); i++ {
+			for x := 0; x < len(m[matches[i]]); x++ {
+				c := m[matches[i]][x]
+				weight += c.Weight
+				result[x] = c
+			}
+		}
+		return weight, &result
+	}
+
 	return 0, nil
+}
+
+func highCard(hand [2]Card, flop [3]Card, turn, river [1]Card) (int, *[5]Card) {
+	cards := concatenate(hand, flop, turn, river)
+	var sum int
+	var result [5]Card
+	for i := 0; i < 5; i++ {
+		sum += cards[i].Weight
+		result[i] = cards[i]
+	}
+	return sum, &result
 }
 
 func groupByID(hand [2]Card, flop [3]Card, turn, river [1]Card) map[string][]Card {
@@ -166,19 +391,23 @@ func groupByID(hand [2]Card, flop [3]Card, turn, river [1]Card) map[string][]Car
 	return m
 }
 
-func highCard(hand [2]Card, flop [3]Card, turn, river [1]Card) int {
+func groupBySuit(hand [2]Card, flop [3]Card, turn, river [1]Card) map[string][]Card {
 	cards := concatenate(hand, flop, turn, river)
-	var sum int
-	for i := 0; i < 5; i++ {
-		sum += cards[i].Weight
+	m := make(map[string][]Card)
+	for i := 0; i < len(cards); i++ {
+		if m[cards[i].Suit] == nil {
+			m[cards[i].Suit] = []Card{cards[i]}
+		} else {
+			m[cards[i].Suit] = append(m[cards[i].Suit], cards[i])
+		}
 	}
-	return sum
+	return m
 }
 
 func concatenate(hand [2]Card, flop [3]Card, turn, river [1]Card) [7]Card {
 	cards := [7]Card{hand[0], hand[1], flop[0], flop[1], flop[2], turn[0], river[0]}
 	sort.Slice(cards[:], func(i, j int) bool {
-		return cards[i].Weight < cards[j].Weight
+		return cards[i].Weight > cards[j].Weight
 	})
 	return cards
 }
